a2d8c0af9399bd03395e77fd319bb183bd8e8ba2
Kevin Zoltany <kzoltany@gmail.com>
2026-05-10 18:06:05 -0700

    Shadcn

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -2,9 +2,14 @@ import "~/styles/globals.css"
 
 import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
-import { Geist } from "next/font/google"
+import { Geist, Inter } from "next/font/google"
 
 import { env } from "~/env"
+import { cn } from "~/lib/utils";
+
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
 	title: "Nxt bot",
@@ -22,7 +27,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-			<html className={`${geist.variable}`} lang="en">
+			<html className={cn(geist.variable, "font-sans", inter.variable, geistHeading.variable)} lang="en">
 				<body>{children}</body>
 			</html>
 		</ClerkProvider>
